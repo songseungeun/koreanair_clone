d7a009660898185773e2fd19083f3a9f350fd59d
songseungeun <grayandjj@gmail.com>
2020-01-30 21:06:38 +0900

    Fixed tab icon, mark up, css.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var menu = $('.quick-booking');
 var items = $('.booking-tab');
 
 items.on('click keyup focus', function(e) {
+    e.preventDefault();
     if (e.type === 'click' || (e.type === 'keyup' && e.keyCode === 13) || e.type === 'focus') {
         items.removeClass('on');
         $(this).addClass('on');
