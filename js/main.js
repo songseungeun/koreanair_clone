ce961c20cc128994272d3bff0db332dc652fa191
songseungeun <grayandjj@gmail.com>
2020-02-02 13:36:11 +0900

    improved access to information

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
 let menu = $('.quick-booking');
 let items = $('.airline-tab, .hotel-tab, .rentacar-tab');
 
-items.on('click keyup focus', function(e) {
+items.on('click keyup', function(e) {
     e.preventDefault();
-    if (e.type === 'click' || (e.type === 'keyup' && e.keyCode === 13) || e.type === 'focus') {
+    if (e.type === 'click' || (e.type === 'keyup' && e.keyCode === 13) ) {
         items.removeClass('on');
         $(this).addClass('on');
+        $(this).attr('tabindex', 0).siblings().attr('tabindex', -1);
     }
 });
 
 
 let tabs = $('.quick-booking-tab [role="tab"]')
 
-tabs.on('click', function (e) {
+tabs.on('click keyup', function (e) {
     e.preventDefault();
+    if (e.type === 'click' || (e.type === 'keyup' && e.keyCode === 13) ) {
     $(this).attr('aria-selected', true).siblings().attr('aria-selected', false);
     let selectedId = "#"+$(this).attr('aria-controls');
     $(selectedId).addClass('detail-view').siblings().removeClass('detail-view');
-})
+    $(selectedId).attr('tabindex', 0).siblings().attr('tabindex', -1);
+    }
+    
+});
