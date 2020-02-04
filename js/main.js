fefc7f6ec44b507761c6c9aa03c0f299c651dcf4
songseungeun <grayandjj@gmail.com>
2020-02-04 22:45:56 +0900

    modify html mark up css style, module.

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ items.on('click keyup', function(e) {
 let tabs = $('.quick-booking-tab [role="tab"]')
 
 tabs.on('click keyup', function (e) {
-    e.preventDefault();
+    // e.preventDefault();
     if (e.type === 'click' || (e.type === 'keyup' && e.keyCode === 13) ) {
     $(this).attr('aria-selected', true).siblings().attr('aria-selected', false);
     let selectedId = "#"+$(this).attr('aria-controls');
@@ -22,4 +22,29 @@ tabs.on('click keyup', function (e) {
     $(selectedId).attr('tabindex', 0).siblings().attr('tabindex', -1);
     }
     
+});
+
+
+
+var button = $('.btn');
+
+button.on('click keyup', function(e) {
+  if (e.type === 'click' || (e.type === 'keyup' && e.keyCode === 13) ) {
+    e.preventDefault();
+      $(this).closest('.btn-grp').children('.btn-on').removeClass('btn-on');
+      $(this).addClass('btn-on');
+      // $(this).addClass('btn-on').siblings().removeClass('btn-on');
+  }
+});
+
+
+
+var booking_detail_menu = $('.airline-ticket, .airline-checkin,.airline-schedule');
+    
+booking_detail_menu.on('click keyup focus', function(e) {
+    if (e.type === 'click' || (e.type === 'keyup' && e.keyCode === 13) || e.type === 'focus') {
+        // e.preventDefault();
+        booking_detail_menu.removeClass('menu-act');
+        $(this).addClass('menu-act');
+    }
 });
